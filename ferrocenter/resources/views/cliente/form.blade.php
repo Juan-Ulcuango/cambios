9ce6fb4569e1beae8f5602f072bd945863390357
Jeremy091 <136599778+Jeremy091@users.noreply.github.com>
2024-06-10 17:59:02 -0500

    botonesCancelar

--- ferrocenter/resources/views/cliente/form.blade.php
+++ ferrocenter/resources/views/cliente/form.blade.php
@@ -57,7 +57,7 @@
     <div class="form-footer">
         <div class="text-end">
             <div class="d-flex">
-                <a href="#" class="btn btn-danger">Cancel</a>
+                <a href="{{ route('clientes.index') }}" class="btn btn-danger">Cancel</a>
                 <button type="submit" class="btn btn-primary ms-auto ajax-submit">Submit</button>
             </div>
         </div>
