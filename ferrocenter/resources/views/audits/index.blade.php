1e5496f0dcaa7462d63fab8dc4c437ac9b9645cf
Juan Ulcuango <ulcuangoulcuango@gmail.com>
2024-07-18 13:04:31 -0500

    Checks Auditoria

--- ferrocenter/resources/views/audits/index.blade.php
+++ ferrocenter/resources/views/audits/index.blade.php
@@ -63,18 +63,6 @@
                             <table class="table card-table table-vcenter text-nowrap datatable">
                                 <thead>
                                     <tr>
-                                        <th class="w-1"><input class="form-check-input m-0 align-middle" type="checkbox"
-                                                aria-label="Select all audits"></th>
-                                        <th class="w-1">No.
-                                            <!-- Download SVG icon from http://tabler-icons.io/i/chevron-up -->
-                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                                class="icon icon-sm text-dark icon-thick" width="24" height="24"
-                                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                                stroke-linecap="round" stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                <polyline points="6 15 12 9 18 15" />
-                                            </svg>
-                                        </th>
                                         <th>ID</th>
                                         <th>Usuario</th>
                                         <th>Evento</th>
@@ -89,9 +77,6 @@
                                 <tbody>
                                     @foreach ($audits as $audit)
                                         <tr>
-                                            <td><input class="form-check-input m-0 align-middle" type="checkbox"
-                                                    aria-label="Select audit"></td>
-                                            <td style="word-wrap: break-word; white-space: pre-wrap;">{{ $loop->iteration }}</td>
                                             <td style="word-wrap: break-word; white-space: pre-wrap;">{{ $audit->id }}</td>
                                             <td style="word-wrap: break-word; white-space: pre-wrap;">{{ optional($audit->user)->name }}</td>
                                             <td style="word-wrap: break-word; white-space: pre-wrap;">{{ $audit->event }}</td>
